6b919d545582069867bb663bfce0f5a74d52ee6d
Osgosi <osgosi92@gmail.com>
2025-06-06 19:57:30 -0600

    Agregada función de df a np

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import numpy as np
 import pandas as pd
 
-print("Hola Mundo")
+def numpy_df():
+    #Create a DataFrame with random data
+    df = pd.DataFrame(np.random(5, 3), columns=['A', 'B', 'C'])
+
+    #Print the DataFrame
+    print("DataFrame:")
+    print(df)
+
+    #Convert DataFrame to Numpy array
+    arr = df.to_numpy()
+
+    #Print the NumPy array
+    print("\nNumPy Array:")
+    print(arr)
+
+numpy_df()
